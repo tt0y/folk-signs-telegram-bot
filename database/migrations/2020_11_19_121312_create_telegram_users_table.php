49c1b98bd0bafe0240849f4a81ed2aba6079fb09
Anton KC <ajusmith@gmail.com>
2020-11-19 15:38:46 +0300

    telegram users table added

---
+++ database/migrations/2020_11_19_121312_create_telegram_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTelegramUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('telegram_users', function (Blueprint $table) {
+            $table->id();
+            $table->string('user_id');
+            $table->string('chat_id');
+            $table->text('data');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('telegram_users');
+    }
+}
