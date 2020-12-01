92a065e062967b9e2ea32411d8430b6d3031a8d2
Anton KC <ajusmith@gmail.com>
2020-12-01 10:20:12 +0300

    superstition on 01.12 updated

--- database/seeders/DefaultUsersSeeder.php
+++ database/seeders/DefaultUsersSeeder.php
@@ -18,7 +18,7 @@ class DefaultUsersSeeder extends Seeder
         User::create([
             'name'              => 'admin@primetki.ru',
             'email'             => 'admin@primetki.ru',
-            'password'          => Hash::make('secret'),
+            'password'          => Hash::make('7bn8vrt'),
             'email_verified_at' => now(),
         ]);
     }
