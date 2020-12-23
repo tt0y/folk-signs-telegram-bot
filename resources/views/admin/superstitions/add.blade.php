77059019f61c2207a226f2401970d6475a891079
Anton KC <ajusmith@gmail.com>
2020-12-23 11:21:13 +0300

    submit button style changed

--- resources/views/admin/superstitions/add.blade.php
+++ resources/views/admin/superstitions/add.blade.php
@@ -1,17 +1,17 @@
 @extends('layouts.admin')
 @section('content')
     <main class="col-sm-9 offset-sm-3 col-md-10 offset-md-2 pt-3">
-        <h1>Добавить примету</h1>
+        <h1>{{__('Добавить примету')}}</h1>
 
         <form method="post">
             {!! csrf_field() !!}
-            <p>День приметки <input type="text" name="day" class="form-control"></p>
-            <p>Месяц приметки <input type="text" name="month" class="form-control"></p>
-            <p>Заголовок приметки <input type="text" name="name" class="form-control"></p>
-            <p>Описание (пойдет в телеграм) <textarea type="text" name="description" class="form-control"></textarea></p>
-            <p>Текст (будет отображаться на сайте) <textarea type="text" name="full_text" class="form-control"></textarea></p>
+            <p>{{__('День приметки')}} <input type="text" name="day" class="form-control"></p>
+            <p>{{__('Месяц приметки')}} <input type="text" name="month" class="form-control"></p>
+            <p>{{__('Заголовок приметки')}} <input type="text" name="name" class="form-control"></p>
+            <p>{{__('Описание (телеграм)')}} <textarea type="text" name="description" class="form-control"></textarea></p>
+            <p>{{__('Текст (статья сайт)')}} <textarea type="text" name="full_text" class="form-control"></textarea></p>
 
-            <button type="submit" class="btn-success">Добавить</button>
+            <button type="submit" class="btn btn-success">{{__('Добавить')}}</button>
         </form>
     </main>
 @endsection
--- resources/views/admin/superstitions/edit.blade.php
+++ resources/views/admin/superstitions/edit.blade.php
@@ -1,17 +1,17 @@
 @extends('layouts.admin')
 @section('content')
     <main class="col-sm-9 offset-sm-3 col-md-10 offset-md-2 pt-3">
-        <h1>Изменить категорию</h1>
+        <h1>{{__('Изменить категорию')}}</h1>
 
         <form method="post">
             {!! csrf_field() !!}
-            <p>День приметки <input type="text" name="day" class="form-control" value="{{ $superstition->day }}"></p>
-            <p>Месяц приметки <input type="text" name="month" class="form-control" value="{{ $superstition->month }}"></p>
-            <p>Заголовок приметки <input type="text" name="name" class="form-control" value="{{ $superstition->name }}"></p>
-            <p>Описание (пойдет в телеграм) <textarea type="text" name="description" class="form-control">{{ $superstition->description }}</textarea></p>
-            <p>Текст (будет отображаться на сайте) <textarea type="text" name="full_text" class="form-control">{{ $superstition->full_text }}</textarea></p>
+            <p>{{__('День приметки')}} <input type="text" name="day" class="form-control" value="{{ $superstition->day }}"></p>
+            <p>{{__('Месяц приметки')}} <input type="text" name="month" class="form-control" value="{{ $superstition->month }}"></p>
+            <p>{{__('Заголовок приметки')}} <input type="text" name="name" class="form-control" value="{{ $superstition->name }}"></p>
+            <p>{{__('Описание (телеграм)')}} <textarea type="text" name="description" class="form-control">{{ $superstition->description }}</textarea></p>
+            <p>{{__('Текст (статья сайт)')}} <textarea type="text" name="full_text" class="form-control">{{ $superstition->full_text }}</textarea></p>
 
-            <button type="submit" class="btn-success">Сохранить</button>
+            <button type="submit" class="btn btn-success">{{__('Сохранить')}}</button>
         </form>
     </main>
 @endsection
