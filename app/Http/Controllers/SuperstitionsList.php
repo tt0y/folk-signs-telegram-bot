2ac455de3d68b2f2f5e182c61b0ce2e5ceea8738
Anton KC <ajusmith@gmail.com>
2021-01-28 22:57:05 +0300

    changed the format of links for the sitemap

--- app/Http/Controllers/SuperstitionsList.php
+++ app/Http/Controllers/SuperstitionsList.php
@@ -9,13 +9,26 @@ use Illuminate\Support\Str;
 
 class SuperstitionsList extends Controller
 {
+    protected $arMonths = array(
+        1 => 'Января',
+        2 => 'Февраля',
+        3 => 'Марта',
+        4 => 'Апреля',
+        5 => 'Мая',
+        6 => 'Июня',
+        7 => 'Июля',
+        8 => 'Августа',
+        9 => 'Сентября',
+        10 => 'Октября',
+        11 => 'Ноября',
+        12 => 'Декабря');
+
     public function index()
     {
         $sups = Superstition::all()->toArray();
 
         foreach ($sups as $sup) {
-            echo '<br><a href="'. URL::to('/') . '/' . $sup['day'] . '/' . $sup['month'] . '/' . Str::slug($sup['name']) . '">'.$sup['name'].'</a>';
-
+            echo '<br><a href="' . URL::to('/') . '/' . $sup['day'] . '/' . $sup['month'] . '/' . Str::slug($sup['name']) . '">' . 'Народные приметы на ' . $sup['day'] . ' ' . $this->arMonths[(int)$sup['month']] . ' ' . $sup['name'] . '</a>';
         }
     }
 }
